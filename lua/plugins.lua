4e9eeba94e88a2ef84f1ec0b3f2ca4799607c62d
Andrew Gibbons <andrewgibbons@Mac.kcn.ne.jp>
2025-05-04 17:22:39 +0900

    Added comment.nvim plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,6 +64,15 @@ require('lazy').setup({
     end,
   },
 
+
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
